924f3464ddab7689cf2527ca36dfcfe7cce0062a
Anant Gupta <anantgupta129@gmail.com>
2022-07-23 23:02:37 +0530

    congiurations

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 from easydict import EasyDict
+from torch import _pin_memory
 
 
 class TrainingConfigurations(EasyDict):
     seed = 69
     number_classes = 100
     batch_size = 128
+    num_workers = 1
+    pin_memory = False
     epochs = 10
     optimizer = "sgd"
     learning_rate = 1e-5
