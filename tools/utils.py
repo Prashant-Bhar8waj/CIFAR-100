7b4818bf9c7cf730e0159cd0e51aa2ae8cea3bbc
Anant Gupta <anantgupta129@gmail.com>
2022-07-23 23:00:52 +0530

    datastats mean and std

--- tools/utils.py
+++ tools/utils.py
@@ -1,11 +1,13 @@
+import json
 import os
 import random
+
 import numpy as np
 import torch
 
 
 def set_seed(seed):
-    #   for REPRODUCIBILITY.
+    # for REPRODUCIBILITY.
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -15,3 +17,25 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
     # Set a fixed value for the hash seed
     os.environ["PYTHONHASHSEED"] = str(seed)
+
+
+def datastats(loader):
+    """Calculate mean and standard deviation of the dataset
+    Args:
+        loader (instance): torch instance for data loader
+    Returns:
+        tensor: mean and std of data
+    """
+    channel_sum, channel_squared_sum, num_batches = 0, 0, 0
+
+    for img, _ in loader:
+        channel_sum += torch.mean(img, dim=[0, 2, 3])
+        channel_squared_sum += torch.mean((img) ** 2, dim=[0, 2, 3])
+        num_batches += 1
+
+    mean = channel_sum / num_batches
+    std = (channel_squared_sum / num_batches - mean**2) ** 0.5
+    print("The mean of dataset : ", mean)
+    print("The std of dataset : ", std)
+    return mean, std
+
