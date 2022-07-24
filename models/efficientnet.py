89349aa3017ca56f879f990786817a7f030e83dc
Anant Gupta <anantgupta129@gmail.com>
2022-07-24 16:05:40 +0530

    added efficient netv2s

---
+++ models/efficientnet.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class EfficientNetV2S(nn.Module):
+    def __init__(self, nc):
+        super(EfficientNetV2S, self).__init__()
+
+        self.model = models.efficientnet_v2_s(pretrained=True)
+
+        num_ftrs = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(num_ftrs, nc)
+
+    def forward(self, x):
+        out = self.model(x)
+
+        return out
+
+
